e760733b51dfa580e12a0cd84ee86d87464d4017
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-03-18 18:09:54 -0500

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
 
  
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://shielded-wildwood-86291.herokuapp.com' } 
+  # 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
