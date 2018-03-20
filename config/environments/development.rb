2d8ecd8fb9135abab2a4eecb535dea09a9602c97
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-03-19 22:48:52 -0500

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,17 +51,18 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   
-  '''
+  
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'shielded-wildwood-86291.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
 
+'''
   config.action_mailer.smtp_settings = {
     domain: => 'shielded-wildwood-86291.herokuapp.com',
     address:              'smtp.gmail.com',
