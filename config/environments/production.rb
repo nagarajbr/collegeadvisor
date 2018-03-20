ec262ac0086f7fbe9e8caeab741a9394ba22e5aa
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-03-19 21:37:54 -0500

    mlr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -146,7 +146,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
+#end
 
   # ActionMailer Config
  # config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
