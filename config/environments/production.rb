8917cfa80aba28fb7d6f19acb631beb672de1791
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-03-19 22:52:21 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ config.i18n.fallbacks = true
 
 # Send deprecation notices to registered listeners.
 config.active_support.deprecation = :notify
-
+'''
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
@@ -77,12 +77,33 @@ config.action_mailer.smtp_settings = {
   user_name: Rails.application.secrets.email_provider_username,
   password: Rails.application.secrets.email_provider_password
 }
+'''
 # ActionMailer Config
 config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 
+# ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'shielded-wildwood-86291.herokuapp.com'} 
+  # 'localhost:3000' }
+  Rails.application.routes.default_url_options[:host] = 'shielded-wildwood-86291.herokuapp.com'
+  
+  config.action_mailer.delivery_method = :smtp
+
+  
+  config.action_mailer.perform_deliveries = true
+  
+  
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 # Use default logging formatter so that PID and timestamp are not suppressed.
 config.log_formatter = ::Logger::Formatter.new
