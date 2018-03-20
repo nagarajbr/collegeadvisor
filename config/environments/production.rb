34eb19bb71d7470d49a031f8ffc314d2c6df278a
nagarajbr <nagaraj.bettadapura@gmail.com>
2018-03-19 21:35:32 -0500

    mlr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -148,12 +148,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-  }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ # config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
