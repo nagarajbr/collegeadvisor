93b6490fbff277dc2d9e2b50d783242b05f120ab
thirumal <thirumal_rao@yahoo.com>
2018-03-26 21:03:46 -0500

    version 7

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,7 +1,7 @@
 module ClientsHelper
 	def get_student_name(arg_id)
 
-		@client = Client.where("id = 3 ", arg_id)
+		@client = Client.where("id = ?", arg_id)
 		@name = @client.first.first_name + ', ' + @client.first.last_name
 		
 	end
